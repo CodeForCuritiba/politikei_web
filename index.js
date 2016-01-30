966477c4ad584001f55b1c2b7c5b26af80e2eff2
Murilo Lima <murilo.wlima@gmail.com>
2016-01-30 13:33:40 -0200

    Added expressjs to project

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+
+app.use(express.static(__dirname + '/app'));
+
+// bind the app to listen for connections on a specified port
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+// Render some console log output
+console.log("Listening on port " + port);
