f429c82e345455f7a680990ff95a53a6dfe2aa4a
Murilo Lima <murilo.wlima@gmail.com>
2016-05-14 16:18:03 -0300

    Add front page layout

--- app/src/public/home/HomeController.js
+++ app/src/public/home/HomeController.js
@@ -3,10 +3,21 @@
 
     var home = angular.module('home');
 
-    home.controller('HomeController', HomeController);
+    home.controller('HomeController', ['$scope', '$rootScope', HomeController]);
 
-    function HomeController() {
+    function HomeController($scope, $rootScope) {
         var self = this;
+
+
+        $rootScope.$on('$includeContentLoaded', function() {
+            JQuery(document).ready(function() {
+                // This command is used to initialize some elements and make them work properly
+                JQuery.material.init();
+            });
+        });
     }
+
+
+
 
 })();
