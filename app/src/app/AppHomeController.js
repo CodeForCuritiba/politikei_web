9dbce07f9236b7a6f515e87af393de265fcb0d0b
Thiago Classen <tgclassen.dev@gmail.com>
2016-08-23 16:25:31 -0300

    -responsive menu toggle.

--- app/src/app/AppHomeController.js
+++ app/src/app/AppHomeController.js
@@ -1,21 +1,24 @@
-(function(){
+(function () {
     'use strict';
 
     angular
         .module('politikei')
         .controller('AppHomeController', AppHomeController);
 
-    AppHomeController.$inject = ['$state'];
-    
-    function AppHomeController($state){
+    AppHomeController.$inject = ['$state', '$mdSidenav'];
+
+    function AppHomeController($state, $mdSidenav) {
         var vm = this;
         init();
 
         function init() {
-            $state.go('.proposicoes');    
-            console.log('teste');
+            $state.go('.proposicoes');
+        }
+
+        this.toggleUsersList = function() {
+            $mdSidenav('left').toggle();
         }
 
     }
 
-}());
+} ());
