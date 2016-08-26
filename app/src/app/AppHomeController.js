2b7fcbf40935424b36d288fcea286f06010b44e9
Thiago Classen <tgclassen.dev@gmail.com>
2016-08-26 00:56:48 -0300

    'sobre' and 'contribua' now redirects to the landing page

--- app/src/app/AppHomeController.js
+++ app/src/app/AppHomeController.js
@@ -5,9 +5,9 @@
         .module('politikei')
         .controller('AppHomeController', AppHomeController);
 
-    AppHomeController.$inject = ['$state', '$mdSidenav'];
+    AppHomeController.$inject = ['$state', '$mdSidenav', '$window'];
 
-    function AppHomeController($state, $mdSidenav) {
+    function AppHomeController($state, $mdSidenav, $window) {
         var vm = this;
         this.selected = '';
         init();
@@ -23,6 +23,20 @@
         this.menuItemSelected = function(option) {
             vm.selected = option;
             $mdSidenav('left').toggle();
+        }
+
+        this.open = function (page) {
+            let url = null;
+            
+            if (page === 'contribua') {
+                url = 'http://localhost:3000';
+            }
+            if (page === 'sobre'){
+                url = 'http://localhost:3000';
+            }
+            if (url !== null){
+                $window.open(url);
+            }
         }
 
     }
