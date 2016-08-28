6f020ed03d4fec2b94bd1a1a5e28ee03b2a81210
Thiago Classen <tgclassen.dev@gmail.com>
2016-08-28 01:20:44 -0300

    temp login modal on /#/demo

--- app/src/public/home/HomeController.js
+++ app/src/public/home/HomeController.js
@@ -1,22 +1,45 @@
-(function() {
+(function () {
     'use strict';
 
     var home = angular.module('home');
 
-    home.controller('HomeController', ['$scope', '$rootScope', HomeController]);
-
-    function HomeController($scope, $rootScope) {
+    home.controller('HomeController', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
         var self = this;
 
-        $scope.content_loaded = function() {
-            $(function() {
+        $scope.content_loaded = function () {
+            $(function () {
                 // This command is used to initialize some elements and make them work properly
                 $.material.init();
 
                 $.init_menu();
             });
-        }
+        };
 
-    }
+        $scope.status = '  ';
 
+        this.showLogin = function (ev) {
+            $mdDialog.show({
+                controller: DialogController,
+                //controllerAS: 'ctrl',
+                templateUrl: 'src/public/home/login-modal.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: true // Only for -xs, -sm breakpoints.
+            })
+                .then(function (answer) {
+                    console.log('You said the information was "' + answer + '".');
+                }, function () {
+                    console.log('You cancelled the dialog.');
+                });
+        };
+
+        DialogController.$inject = ['$scope', '$mdDialog'];
+        
+        function DialogController($scope, $mdDialog) {
+           $scope.submit = function (answer) {
+                $mdDialog.hide(answer);
+            };
+        };
+    }]);
 })();
