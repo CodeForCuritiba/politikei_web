b2530bba31b556b7108496249b5240460874596d
Douglas Schmidt <douglaslondrina@gmail.com>
2016-09-01 16:29:33 -0300

    [wip] ranking layout ongoing; [fix] minor proposals layout adj.

--- app/src/app/ranking/RankingService.js
+++ app/src/app/ranking/RankingService.js
@@ -21,27 +21,36 @@
             },
             mockRankingData: function() {
                 // Ranks:
-                return $q.when([
+                var result = $q.when([
                 {
                     parlamentar: {
                         avatar_url: 'http://cicerocattani.com.br/wp-content/uploads/2015/03/jonny-Stica.jpg',
-                        nome: 'Johnny Stica'
+                        nome: 'Johnny Stica',
+                        partido: 'PDL',
+                        numero: '123',
+                        propostas_votadas: 28
                     },
                     match_votos_favor: 11,
                     match_votos_contra: 12,
                     match_proposicoes: 25,
-                    total_proposicoes: 30
+                    total_proposicoes: 30,
+                    perc_positivo: 30
                 },
                 {
                     parlamentar: {
                         avatar_url: 'http://cicerocattani.com.br/wp-content/uploads/2015/03/jonny-Stica.jpg',
-                        nome: 'Johnny Walker'
+                        nome: 'Johnny Walker',
+                        partido: 'PDLs',
+                        numero: '1234',
+                        propostas_votadas: 24
                     },
                     match_votos_favor: 6,
                     match_votos_contra: 13,
                     match_proposicoes: 22,
                     total_proposicoes: 30
                 }]);
+
+                return result;
             }
         };
     }
